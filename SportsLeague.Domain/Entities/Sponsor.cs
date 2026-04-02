182037b4775c6795780314cd1bf28e349308e3cf
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-04-02 15:36:20 -0500

    implement Sponsor entity with required properties

---
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -0,0 +1,15 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Entities;
+
+public class Sponsor
+{
+    public int Id { get; set; } // PK, autoincremental 
+    public string Name { get; set; } = null!; // Nombre del patrocinador 
+    public string ContactEmail { get; set; } = null!; // Email de contacto 
+    public string? Phone { get; set; } // Teléfono (Opcional) 
+    public string? WebsiteUrl { get; set; } // Sitio web (Opcional) 
+    public SponsorCategory Category { get; set; } // Enum: categoría 
+    public DateTime CreatedAt { get; set; } = DateTime.Now; // Fecha de creación 
+    public DateTime? UpdatedAt { get; set; } // Fecha de actualización 
+}
