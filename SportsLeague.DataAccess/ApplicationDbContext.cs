5abbf924c268c8dcfa00fa93e50a5dea9e78f5ba
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-04-02 15:59:43 -0500

    add TournamentSponsor entity for N:M relationship

--- SportsLeague.DataAccess/ApplicationDbContext.cs
+++ SportsLeague.DataAccess/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SportsLeague.Domain.Entities;
 
+
 namespace SportsLeague.DataAccess;
 
 public class ApplicationDbContext : DbContext
@@ -13,7 +14,8 @@ public class ApplicationDbContext : DbContext
     public DbSet<Referee> Referees { get; set; }
     public DbSet<Tournament> Tournaments { get; set; }
 
-
+    public DbSet<Sponsor> Sponsors { get; set; }
+    public DbSet<TournamentSponsor> TournamentSponsors { get; set; } 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -23,5 +25,20 @@ public class ApplicationDbContext : DbContext
         modelBuilder.Entity<Team>()
             .HasIndex(t => t.Name)
             .IsUnique();
+
+        // Regla de negocio: El nombre del patrocinador debe ser único
+        modelBuilder.Entity<Sponsor>()
+            .HasIndex(s => s.Name)
+            .IsUnique();
+
+        // Configuración para TournamentSponsor
+        modelBuilder.Entity<TournamentSponsor>()
+            .HasIndex(ts => new { ts.TournamentId, ts.SponsorId })
+            .IsUnique(); // Índice compuesto único (Punto 4.4 - Criterio 1)
+
+        modelBuilder.Entity<TournamentSponsor>()
+            .Property(ts => ts.ContractAmount)
+            .HasPrecision(18, 2); // Configuración para decimales
+
     }
 }
