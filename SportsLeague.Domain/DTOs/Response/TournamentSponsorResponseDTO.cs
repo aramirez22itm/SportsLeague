4efa95e35523480fc43b7d84aacae9f9d44baa92
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-04-02 22:53:35 -0500

    chore: deep clean of build artifacts to resolve persistent CS0101 naming conflicts

--- SportsLeague.Domain/DTOs/Response/TournamentSponsorResponseDTO.cs
+++ SportsLeague.Domain/DTOs/Response/TournamentSponsorResponseDTO.cs
@@ -1,6 +1,7 @@
 ﻿namespace SportsLeague.Domain.DTOs.Response
 {
     public class TournamentSponsorResponseDTO
+
     {
         public int TournamentId { get; set; }
         public int SponsorId { get; set; }
