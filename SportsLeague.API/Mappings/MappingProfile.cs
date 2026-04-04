ccd04414207832439c207dea0b64c85a962a8b1f
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-04-03 20:00:20 -0500

    chore: temporarily remove TournamentSponsor DTOs to isolate CS0101 naming conflict

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -3,8 +3,7 @@ using SportsLeague.Domain.DTOs;
 using SportsLeague.Domain.DTOs.Request;
 using SportsLeague.Domain.DTOs.Response;
 using SportsLeague.Domain.Entities;
-using SportsLeague.Domain.DTOs.Request;
-using SportsLeague.Domain.DTOs.Response;
+
 public class MappingProfile : Profile
 {
     public MappingProfile()
@@ -12,13 +11,13 @@ public class MappingProfile : Profile
         // Esto le dice: "Puedes convertir un Team en un TeamDto automáticamente"
         CreateMap<Team, TeamDto>().ReverseMap();
 
-        // Mapeos para Sponsor (Requerimiento 4.4.6)
+        // Mapeos para Sponsor 
         CreateMap<Sponsor, SponsorResponseDTO>(); // De Entidad a Respuesta
         CreateMap<SponsorRequestDTO, Sponsor>(); // De Solicitud a Entidad
 
         // Mapeos para TournamentSponsor
-        CreateMap<TournamentSponsor, TournamentSponsorResponseDTO>();
-        CreateMap<TournamentSponsorRequestDTO, TournamentSponsor>();
+        // CreateMap<TournamentSponsor, TournamentSponsorResponseDTO>().ReverseMap();
+        // CreateMap<TournamentSponsorRequestDTO, TournamentSponsor>().ReverseMap();
 
     }
 }
--- SportsLeague.Domain/DTOs/Request/TournamentSponsorRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SportsLeague.Domain.DTOs.Request
-{
-    public class TournamentSponsorRequestDTO
-    {
-        public int TournamentId { get; set; }
-        public int SponsorId { get; set; }
-    }
-}
