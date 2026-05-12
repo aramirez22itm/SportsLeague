17219cfb5b79b34dc5a091a213e58ed5f939d7cb
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-05-11 20:49:35 -0500

    update the cors in aplication

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -42,7 +42,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -53,6 +59,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
